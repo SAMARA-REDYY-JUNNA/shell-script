1a87bdf0152efae2ec9c085e93e674282592e511
samarareddy <samara.junna@gmail.com>
2024-05-01 23:02:42 +0530

    first commit, my code

--- 06-variable.sh
+++ 06-variable.sh
@@ -7,4 +7,6 @@ MOVIES=("RRR", "sam","pokiri")
 
 #list always starts with 0.
 
-echo "first movie is ${MOVIE[0]}"
+echo "first movie is ${MOVIES[0]}"
+echo "first movie is ${MOVIES[1]}"
+echo "first movie is ${MOVIES[@]}"
