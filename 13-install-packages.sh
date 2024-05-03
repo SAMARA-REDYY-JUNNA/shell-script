8845d964272b2c540b5bdb024476017896867c07
samarareddy <samara.junna@gmail.com>
2024-05-03 18:49:20 +0530

    same- message

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,7 +1,11 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -27,7 +31,7 @@ do
      dnf list installed $i &>>$LOGFILE
      if [ $? -eq 0 ]
      then
-       echo "$i already installed..SKIPPING"
+       echo -e "$i already installed..$Y SKIPPING $N"
      else
        echo "$i not installed..need to install"
      fi    
