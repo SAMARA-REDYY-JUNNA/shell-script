7613f2ae20e840606300daf9d94037991e4354df
samarareddy <samara.junna@gmail.com>
2024-05-03 13:59:55 +0530

    same- message

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -2,3 +2,10 @@
 
 echo "All variables: $@"
 echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $Home"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "process id of the current script: $$"
+echo "process id of last backgroung command: $!
