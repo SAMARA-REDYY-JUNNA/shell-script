f861b51ddf2ea96d1f3480476fac0d56f3fd6aa9
samarareddy <samara.junna@gmail.com>
2024-05-06 12:36:40 +0530

    same- message

---
+++ 14-current-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "before calling other script, course: $COURSE"
+echo "process iD of current  shell script: $$"
+
+#./15-other-script.sh
+
+echo "after calling other script, course: $COURSE"
