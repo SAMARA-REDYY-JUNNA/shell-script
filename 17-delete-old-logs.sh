b4611d8e52009c4f2c50ca9609a8a4ceb33eb77a
samarareddy <samara.junna@gmail.com>
2024-05-06 17:16:11 +0530

    same- message

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 SOURCE_DIRECTORY=/tmp/app-logs
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 if [ -d $SOURCE_DIRECTORY ]
 then
     echo -e " $G source directory exists $N"
@@ -13,6 +15,6 @@ else
     exit 1
  fi
 
- FILES=$(find $SOURCE_DIRECTORY -name "*.log -mtime +14)
+ FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 
  echo "files to delete: $FILES"
