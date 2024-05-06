c8957d30401d3e05f27f676f224b0b5a584b686f
samarareddy <samara.junna@gmail.com>
2024-05-06 13:01:47 +0530

    same- message

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -2,9 +2,9 @@
 
 COURSE="Devops from current script"
 
-echo "before calling other script, course: $COURSE"
-echo "process iD of current  shell script: $$"
+echo "Before calling other script, course: $COURSE"
+echo "Process iD of current  shell script: $$"
 
 ./15-other-script.sh
 
-echo "after calling other script, course: $COURSE"
+echo "After calling other script, course: $COURSE"
--- 15-other-script.sh
+++ 15-other-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-COURSE="devops from other script"
+COURSE="Devops from other script"
 
-echo "vraiable value from other script: $COURSE"
-echo "process ID of otther script: $$"
+echo "Variable value from other script: $SOURCE"
+echo "Process ID of other script: $$"
 
