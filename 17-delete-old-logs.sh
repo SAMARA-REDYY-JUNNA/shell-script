97718f00ae05f01c3dca28003344d40fe1b89340
samarareddy <samara.junna@gmail.com>
2024-05-06 16:41:17 +0530

    same- message

--- 17-delete-old-logs.sh
+++ 17-delete-old-logs.sh
@@ -9,6 +9,6 @@ if [ -d $SOURCE_DIRECTORY ]
 then
     echo -e " $G source directory exists $N"
 else
-    echo -e "$R please make sure $SOURCE_DIRECTORY exists $N 
+    echo -e "$R please make sure $SOURCE_DIRECTORY exists $N" 
     exit 1
- fi   
+ fi
